6466de79693037be6e8fa8e31f31e3c723e619c3
Jann Wickert <jann@thomas-wickert.de>
2017-04-13 09:34:30 +0200

    Transferpush -> incomplete

--- app/src/main/java/com/example/leon/carcompanion/CallActivity.java
+++ app/src/main/java/com/example/leon/carcompanion/CallActivity.java
@@ -1,13 +1,191 @@
 package com.example.leon.carcompanion;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.speech.RecognizerIntent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class CallActivity extends AppCompatActivity {
+    Button sendSMS;
+    ImageButton btnCallSpeak;
+    EditText phoneNumber;
+    EditText smsMessage;
+    private String answer;
+    private String text = "";        //Text for Empfänger or Message
+    private static final int PERMISSION_REQUEST = 1;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
+
+        sendSMS = (Button) findViewById(R.id.sendSMS);
+        btnCallSpeak = (ImageButton) findViewById(R.id.btnCallSpeak);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        smsMessage = (EditText) findViewById(R.id.smsMessage);
+
+        sendSMS.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v){
+                String phone = phoneNumber.getText().toString();
+                String message = smsMessage.getText().toString();
+                sendSMS(phone, message);
+            }
+        });
+
+        btnCallSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                promptSpeechInput();
+            }
+        });
+    }
+
+    private void sendSMS(String phone, String message)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.SEND_SMS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // permission is not granted, ask for permission:
+                requestPermissions(new String[] { Manifest.permission.SEND_SMS},
+                        PERMISSION_REQUEST);
+            }
+        }
+
+        Log.println(Log.DEBUG, "Telefonnummer: ", phone);
+        Log.println(Log.DEBUG, "Nachricht: ", message);
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+                new Intent(this, SMSActivity.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phone, null, message, pi, null);
+    }
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED){
+            //txtSpeechInput.setText("Permission Granted");
+        }
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if(data == null){
+                    //txtSpeechInput.setText("No Speech input recognized");
+                }
+
+                /**
+                 if (resultCode == RESULT_OK && null != data) {
+                 ArrayList<String> result = data
+                 .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                 if(result.get(0).toUpperCase().equals("SENDEN")){
+                 String phone = phoneNumber.getText().toString();
+                 String message = smsMessage.getText().toString();
+                 sendSMS(phone, message);
+                 }else if(result.get(0).toUpperCase().equals("EMPFÄNGER")){
+                 phoneNumber.setText(result.get(0));
+                 }else if(result.get(0).toUpperCase().equals("NACHRICHT")){
+                 smsMessage.setText(result.get(0));
+                 }
+
+                 //answer = result.get(0);
+                 } */
+
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    //txtSpeechInput.setText(result.get(0));
+                    answer = result.get(0);
+
+                    for (String s:result) {
+                        Log.d("MAIN", s);
+                    }
+
+                    boolean found = false; //Prüfvariable
+
+                    /*
+                    Die ArrayList @result hat mehrere Antworten. Diese werden jeweils in die
+                    einzelnen Worte aufgeteilt und dann auf definierte Schlagwörter geprüft
+                     */
+                    for (String s:result) { //Aufteilung in "Interpretationen"
+                        String[] split = s.split(" ");
+                        for (String wort:split) { //Aufteilung in die einzelnen Wörter
+
+                            Log.d("MAIN", wort);
+                            switch (wort.toUpperCase()){
+                                case "EMPFÄNGER":
+                                    found = true;
+                                    text = "";
+                                    text = s.substring(10, s.length());
+                                    phoneNumber.setText(text);
+                                    break;
+                                case "NACHRICHT":
+                                    found = true;
+                                    text = "";
+                                    text = s.substring(10, s.length());
+                                    smsMessage.setText(text);
+                                    break;
+                                case "SENDEN":
+                                    found = true;
+                                    if(phoneNumber!=null && smsMessage!=null){
+                                        String phone = phoneNumber.getText().toString();
+                                        String message = smsMessage.getText().toString();
+                                        sendSMS(phone, message);
+                                    }
+                                    break;
+                            }
+                            if (found) break; //Aus der Schleife
+                        }
+
+                        if (found) break;
+                    }
+                }
+
+                break;
+            }
+        }
     }
 }
